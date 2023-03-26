5a6a39be6aad0f031b41d75a6592866f7ac8114c
Gumi <clashroyaleixo@gmail.com>
2023-03-26 09:07:45 +0200

    Add metadata detection

--- sudoku_parser.py
+++ sudoku_parser.py
@@ -11,10 +11,21 @@ class SudokuBoard:
         if sudoku_fen == "":
             raise ValueError
 
-    def get_matrix(self, sudoku_fen: str) -> numpy.ndarray:
+    def get_metadata(self, sudoku_fen: str) -> list:
         fen_list = sudoku_fen.split("/")
-        dimensions = fen_list.pop(0).replace("0", "")
-        tuple_dimensions = (int(dimensions[0]), int(dimensions[1]))
-        print(f"DIMENSIONS OF THE GRID: {tuple_dimensions}")
-        matrix = numpy.reshape(fen_list, tuple_dimensions)
+        puzzle_name = fen_list.pop(0)
+        author_name = fen_list.pop(0)
+        puzzle_size = fen_list.pop(0).replace("0", "")
+        metadata = [
+            puzzle_name,
+            author_name,
+            (int(puzzle_size[0]), int(puzzle_size[1])),
+        ]
+        return metadata
+
+    def get_matrix(self, sudoku_fen: str) -> numpy.ndarray:
+        metadata = self.get_metadata(sudoku_fen)
+        fen_list = sudoku_fen.split("/")[3:]
+        print(f"DIMENSIONS OF THE GRID: {metadata[2]}")
+        matrix = numpy.reshape(fen_list, metadata[2])
         return matrix
