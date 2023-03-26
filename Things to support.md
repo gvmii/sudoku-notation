6c12e7a327567007b035a3da539a477fbdad9785
Gumi <clashroyaleixo@gmail.com>
2023-03-26 08:06:47 +0200

    Started sudoku parser work

---
+++ Things to support.md
@@ -0,0 +1,39 @@
+Extra region (3x3 cage)
+Thermometer (Same as a cage but with a starting cell)
+Palindrome line (Same as a cage)
+Cage (Array of cells)
+Killer sum/Diagonal sum (Diagonal line, has to be notated outside the sudoku)
+Sandwhich sum (Same as a Killer sum)
+Kropki dots (White and black)
+Arrow (Cage)
+Between line (Cage with circles in the extremes)
+Quadruple (Diagonal Kropki dot)
+XV (Kropki dot)
+
+A lot of 1x1 markings:
+Odd
+Even
+Clone
+Minimum
+Maximum
+Shaded Cells
+
+Sudoku extreme variants:
+Disjointed sudoku (https://www.youtube.com/watch?v=EjfiiZ4dSPA)
+Whatever this is: (https://youtu.be/fU6H11tRCmM) - Can be done with null tiles (tiles that don't exist (separate marking))
+
+Cosmetic tiles:
+1x1:
+Text (Position in the subgrid of a tile )
+Circle
+Rectangle
+Custom stuff (Just a modifier, the app can give it an svg or whatever)
+
+Cage:
+Line
+Cage
+Colors (For regions)
+
+Given digits/blank tiles
+
+Hard thing to think about: https://youtu.be/-lWvjDPIl3A
--- sudoku_parser.py
+++ sudoku_parser.py
@@ -2,49 +2,66 @@
 Parses the sudoku_fen string in order to display the appropriate board
 """
 import re
+import numpy
 
 
 class SudokuBoard:
     def __init__(self, sudoku_fen):
-        self.board = self.load_board(sudoku_fen)
+        # self.board = self.load_board(sudoku_fen)
 
-    def load_board(self, sudoku_fen):
-        # Takes out leading numbers from fen (e.g. 0909)
-        if len(sudoku_fen < 4) or sudoku_fen[:4] == "0000":
-            raise ValueError('Input string is too short')
-            num_of_rows, num_of_cols = [
-                int(sudoku_fen[:2].lstrip("0")),
-                int(sudoku_fen[2:4].lstrip("0")),
-            ]
+        VALID_MODIFIERS = ["K", "C", "G", "S", "L"]
 
-        # Regex pattern for modifiers 
-        pattern = r"([KCGSL]\([wbrd]?\d+\))"
+    # def load_board(self, sudoku_fen):
+    #     # Takes out leading numbers from fen (e.g. 0909)
+    #     if len(sudoku_fen < 4) or sudoku_fen[:4] == "0000":
+    #         raise ValueError("Input string is too short")
+    #         num_of_rows, num_of_cols = [
+    #             int(sudoku_fen[:2].lstrip("0")),
+    #             int(sudoku_fen[2:4].lstrip("0")),
+    #         ]
 
-        matches = re.findall(pattern, sudoku_fen)
+    def get_matrix(self, sudoku_fen):
+        fen_list = sudoku_fen.split("/")
+        dimensions = fen_list.pop(0).replace("0", "")
+        tuple_dimensions = (int(dimensions[0]), int(dimensions[1]))
+        print(f"DIMENSIONS OF THE GRID: {tuple_dimensions}")
+        matrix = numpy.reshape(fen_list, tuple_dimensions)
+        return matrix
 
-        # Ensure that tokens start only with KCGSL
-        for match in matches:
-            if not match.startswith(('K', 'C', 'G', 'S', 'L')):
-                raise ValueError(f"Invalid token: {match}")
+        # # Regex pattern for modifiers
+        # pattern = rf"([{self.VALID_MODIFIERS}]\([wbrd]?\d+\))"
 
-        # Replace pattern with 'P' (Placeholder)
-        sudoku_fen = re.sub(pattern, "P", sudoku_fen)
-        parsed_sudoku_fen = []
-        match_counter = 0
-        for i in sudoku_fen[4:]:
-            if i.isdigit():
-                parsed_sudoku_fen.append(i)
-            elif i == "P":
-                parsed_sudoku_fen.append(matches[match_counter])
-                match_counter += 1
-            else:
-                raise ValueError(f"Invalid character: {i}")
-        if len(parsed_sudoku_fen) != (num_of_rows * num_of_cols):
-            raise ValueError("Length of string must equal dimensions")
+        # matches = re.findall(pattern, sudoku_fen)
 
-        board = [
-            parsed_sudoku_fen[i : i + num_of_cols]
-            for i in range(0, num_of_rows * num_of_cols, num_of_cols)
-        ]
+        # # Ensure that tokens start only with KCGSL
+        # for match in matches:
+        #     if not match.startswith(self.VALID_MODIFIERS):
+        #         raise ValueError(f"Invalid token: {match}")
 
-        return board
+        # # Replace pattern with 'P' (Placeholder)
+        # sudoku_fen = re.sub(pattern, "P", sudoku_fen)
+        # parsed_sudoku_fen = []
+        # match_counter = 0
+        # for i in sudoku_fen[4:]:
+        #     if i.isdigit():
+        #         parsed_sudoku_fen.append(i)
+        #     elif i == "P":
+        #         parsed_sudoku_fen.append(matches[match_counter])
+        #         match_counter += 1
+        #     else:
+        #         raise ValueError(f"Invalid character: {i}")
+        # if len(parsed_sudoku_fen) != (num_of_rows * num_of_cols):
+        #     raise ValueError("Length of string must equal dimensions")
+
+        # board = [
+        #     parsed_sudoku_fen[i : i + num_of_cols]
+        #     for i in range(0, num_of_rows * num_of_cols, num_of_cols)
+        # ]
+
+        # return board
+
+
+if __name__ == "__main__":
+    fen = """0909/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0"""
+    board = SudokuBoard(fen)
+    print(board.get_matrix(fen))
