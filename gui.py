54ca7ce539e6647ff82468f2e2282db5184fc65a
Isaac <isaacwritescode7@gmail.com>
2023-03-26 02:43:13 -0500

    Rough draft for gui...really rough lol

--- gui.py
+++ gui.py
@@ -1,19 +1,54 @@
-""" Turns program into graphical interface """
+from PIL import Image, ImageDraw, ImageFont
+import SudokuBoard
 
-from gooey import Gooey, GooeyParser
-from sudoku_parser import SudokuBoard
+class DisplayMatrix:
+    def __init__(self, matrix):
+        # TODO: Fix this mess lol
+        self.sudoku_board = SudokuBoard('0909/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0')
+        self.HEIGHT, self.WIDTH = 1080, 1080
+        self.num_of_rows = num_of_rows 
+        self.num_of_cols = num_of_cols 
 
-@Gooey(footer_bg_color="#789CA4", sidebar_bg_czolor="#789CA4", body_bg_color="#789CA4", header_bg_color="#789CA4")
-def parse_args():
-    parser = GooeyParser(description="Sudoku Board Parser")
-    parser.add_argument("sudoku_fen", help="The FEN string for the Sudoku board")
-    return parser.parse_args()
+    def draw_matrix(self):
+        # calculate the optimal cell size based on the image dimensions and the number of rows/columns
+        if self.num_of_rows > self.num_of_cols:
+            cell_size = self.HEIGHT // num_of_rows
+            width = cell_size * self.num_of_cols
+        else:
+            cell_size = self.WIDTH // self.num_of_cols  # fixed
+            width = cell_size * self.num_of_cols  # added
 
-def main():
-    args = parse_args()
-    sudoku_fen = args.sudoku_fen
-    board = SudokuBoard(sudoku_fen)
-    print(board.board)
 
-if __name__ == "__main__":
-    main()
+        # create a blank image
+        image = Image.new("RGB", (self.WIDTH, self.HEIGHT), color="white")
+
+        # get the font to use for the numbers
+        font = ImageFont.truetype("Arial.ttf", cell_size // 2)
+
+        # create an ImageDraw object
+        draw = ImageDraw.Draw(image)
+
+        # draw the grid lines
+        line_thickness = max(1, cell_size // 20) # adjust line thickness based on cell size
+        for i in range(self.num_of_rows + 1):
+            # horizontal lines
+            y = i * cell_size
+            draw.line([(0, y), (self.WIDTH, y)], fill=(0, 0, 0), width=line_thickness)
+
+        for j in range(self.num_of_cols + 1):
+            # vertical lines
+            x = j * cell_size
+            draw.line([(x, 0), (x, self.HEIGHT)], fill=(0, 0, 0), width=line_thickness)
+
+        # draw the numbers on the image
+        for row in range(self.num_of_rows):
+            for col in range(self.num_of_cols):
+                num = self.sudoku_board[row][col]
+                if num != 0:
+                    x = col * cell_size + cell_size // 4
+                    y = row * cell_size
+                    draw.text((x, y), str(num), font=font, fill=(0, 0, 0))
+        image.show()
+
+x = DisplayMatrix([[1 for i in range(9)] for j in range(9)])
+x.draw_matrix()
